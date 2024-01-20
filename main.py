8dff9b4e67da60dfd7cd7387019b9a45c1d03541
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 18:34:31 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import flet as fl
 
 
+def open_menu() -> None:
+    print(1)
+
+
 def main(page: fl.Page) -> None:
     page.title = "Scientific Calculator"
     page.window_width = 400
     page.window_center()
     page.window_resizable = False
+    page.add(
+        controls=fl.Row(
+            [
+                fl.Text("Scientific Calculator", size=25, color=fl.colors.WHITE),
+                fl.Container(width=page.window_width),
+                fl.IconButton(
+                    icon=fl.Icon(fl.icons.GRID_3X3_ROUNDED, size=15), on_click=open_menu
+                ),
+            ]
+        )
+    )
     page.update()
 
 
