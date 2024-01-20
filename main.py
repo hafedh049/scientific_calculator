5ee1f40d6aaf7014b4b35e341f4213abb90434af
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 17:50:29 +0100

    Modified  main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import flet as fl
 
 
 def main(page: fl.Page) -> None:
-    ...
+    page.title = "Scientific Calculator"
+    page.update()
 
 
 if __name__ == "__main__":
     fl.app(
         target=main,
         assets_dir="assets/",
-        title="Scientific Calculator",
         name="Scientific Calculator",
         use_color_emoji=True,
         view=fl.AppView.FLET_APP,
